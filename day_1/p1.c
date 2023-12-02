2c00c8854eaa5f3e335b763a0e0e5d08ecf990d7
birblett <bwee.sylv@gmail.com>
2023-12-01 18:44:08 -0800

    change fgets arg for disgusting bit hack one liner

--- day_1/p1.c
+++ day_1/p1.c
@@ -4,7 +4,7 @@ int main(int argc, char* argv) {
     FILE* f = fopen("in.txt", "r");
     char buf[8192], *c, tmp, b;
     int cal = 1, last = 0;
-    while ((cal += last) && fgets((c = buf), 100, f) && !(b = 0)) while (tmp = *c++) if (tmp > 47 && tmp < 58 && ((last = tmp - 48) + 1) && !b++) cal += 10 * last;
+    while ((cal += last) && fgets((c = buf), 8192, f) && !(b = 0)) while (tmp = *c++) if (tmp > 47 && tmp < 58 && ((last = tmp - 48) + 1) && !b++) cal += 10 * last;
     printf("%d\n", cal - 1);
     fclose(f);
     return 0;
