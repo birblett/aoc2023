f24154456cfd8c6f832d2f19ecfc976df8f49a20
birblett <bwee.sylv@gmail.com>
2023-12-09 15:23:53 -0800

    small improvement on day 9 algorithm runtime bound

--- day_9/p1.c
+++ day_9/p1.c
@@ -7,7 +7,7 @@ int main() {
     char buf[8192], *c, tmp;
     while(fgets(buf, 8192, f)) {
         len = sgetints(buf, nums, 100);
-        for (i = len - 1; i > 0; --i) 
+        for (i = len - 1; (nums[0] || nums[i]) && i > 0; --i) 
             for (j = 0; j < i; ++j) nums[j] = nums[j + 1] - nums[j];
         for (i = 0; i < len; i++) total += nums[i];
     }
--- day_9/p2.c
+++ day_9/p2.c
@@ -3,12 +3,11 @@
 
 int main() {
     FILE* f = fopen("in.txt", "r");
-    int nums[100], i, j, store, len = 0, total = 0;
+    int nums[100], i, j, store, len = 0, total = 0, start, end;
     char buf[8192], *c, tmp;
     while(fgets(buf, 8192, f)) {
-        for ((len = sgetints(buf, nums, 100)) && (store = i = 0); i < len - 1; ++i) 
-            for (j = len - 1; j > i; --j) 
-                nums[j] -= nums[j - 1];
+        for ((len = sgetints(buf, nums, 100)) && (store = i = 0); (nums[len - 1] || nums[i]) && i < len - 1; ++i) 
+            for (j = len - 1; j > i; --j) nums[j] -= nums[j - 1];
         for (i = len - 1; i > 0; --i) store = nums[i] - store;
         total += nums[0] - store;
     }
